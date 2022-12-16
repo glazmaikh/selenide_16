d1c72dd8ba7b7c365a95ad8ec101440425eb980c
Sergey Orlovskiy <sergey.orlovskiy667@gmail.com>
2022-12-16 19:56:14 +0300

    add configuration.remote selenoid

--- src/test/java/github/TestBase.java
+++ src/test/java/github/TestBase.java
@@ -5,14 +5,15 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
     @BeforeAll
     static void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C://webdrivers/chromedriver.exe");
         Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1932x1160";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @BeforeEach
